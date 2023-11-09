00392db5901e0e1f15f07f4a1998945338d443ba
prasant <praspacific55@gmail.com>
2023-11-09 10:21:17 +0545

    main added

---
+++ main.js
@@ -0,0 +1,16 @@
+//const form =document.querySelector("form");
+//fileInput = form.querySelector(".file-input");
+//progressArea = document.querySelector(".progress-area");
+//uploadedArea = document.querySelector(".uploaded-area");
+//
+//form.addEventListener("click", ()=>{
+    //fileInput.click();
+//});
+
+//f//ileInput.onchange = ({target})=>{
+ //   let file= target.files[0];
+ //   if(file){
+ //       let fileName =file.name;
+ //       uploadFile(fileName);
+//    }
+//} 
